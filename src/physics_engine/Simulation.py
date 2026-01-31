8f9a53def470f497704ccfc4bf371e5d94e666b0
FelipePassarela <126916744+FelipePassarela@users.noreply.github.com>
2026-01-31 18:07:17 -0300

    feat(simulation): implement Simulation class for particles and forces integration
    Added a new Simulation class to manage particle interactions, including gravity, drag, and spring forces.

---
+++ src/physics_engine/Simulation.py
@@ -0,0 +1,63 @@
+import random as rd
+import time
+
+from pyglm import glm
+
+from physics_engine.force.DragForce import DragForce
+from physics_engine.force.GravityForce import GravityForce
+from physics_engine.force.ParticleForceRegistry import ParticleForceRegistry
+from physics_engine.force.SpringForce import SpringForce
+from physics_engine.Particle import Particle
+
+
+class Simulation:
+    def __init__(self):
+        self.registry: ParticleForceRegistry = ParticleForceRegistry()
+        self.particles: list[Particle] = []
+        self.SCALE = 60
+        self._last_time = time.time()  # to calculate delta time
+
+        self._setup_particles()
+
+    def _setup_particles(self):
+        for _ in range(2):
+            position = glm.vec3(
+                self.SCALE * rd.random(),
+                self.SCALE * rd.random(),
+                self.SCALE * rd.random(),
+            )
+            mass = 10 * self.SCALE * rd.random()
+            particle = Particle(position=position, mass=mass)
+            self.particles.append(particle)
+
+        for particle in self.particles:
+            other = (
+                self.particles[1]
+                if particle is self.particles[0]
+                else self.particles[0]
+            )
+            forces = [
+                GravityForce(gravity=glm.vec3(0, -20, 0)),
+                DragForce(k1=0.1, k2=0.01),
+                SpringForce(other=other, k=10, rest_length=3),
+            ]
+            self.registry.add(particle=particle, forces=forces)
+
+    def update(self):
+        now = time.time()
+        dt = now - self._last_time
+        self._last_time = now
+
+        self.registry.update(dt)
+        self.handle_collision()
+
+    def handle_collision(self):
+        for particle, _ in self.registry.get_registry():
+            for axis in range(len(particle.position)):
+                # TODO: Make box constraint by walls with infinite mass
+                if particle.position[axis] < 0:
+                    particle.position[axis] = 0
+                    particle.velocity[axis] *= -1
+                if particle.position[axis] > self.SCALE - 1:
+                    particle.position[axis] = self.SCALE - 1
+                    particle.velocity[axis] *= -1
--- src/physics_engine/__main__.py
+++ src/physics_engine/__main__.py
@@ -1,34 +1,17 @@
 import argparse
-import random
 from functools import partial
 
 from pyglm import glm
 
 from physics_engine.AnimatedPlot import AnimatedPlot
-from physics_engine.force.DragForce import DragForce
-from physics_engine.force.GravityForce import GravityForce
-from physics_engine.Particle import Particle
-from physics_engine.force.ParticleForceRegistry import ParticleForceRegistry
+from physics_engine.Simulation import Simulation
 
 
-def update(
-    registry: ParticleForceRegistry, size: int, dt: float
-) -> list[dict[str, glm.vec3]]:
-    registry.update(dt)
-    plot_data = []
-
-    for particle, _ in registry.get_registry():
-        for axis in range(len(particle.position)):
-            # TODO: Make box constraint by walls with infinite mass
-            if particle.position[axis] < 0:
-                particle.position[axis] = 0
-                particle.velocity[axis] *= -1
-            if particle.position[axis] > size - 1:
-                particle.position[axis] = size - 1
-                particle.velocity[axis] *= -1
-
+def update(simulation: Simulation) -> list[dict[str, glm.vec3]]:
+    simulation.update()
+    plot_data: list[dict] = []
+    for particle in simulation.particles:
         plot_data.append({"point": particle.position})
-
     return plot_data
 
 
@@ -43,27 +26,12 @@ def main() -> None:
     )
     args = parser.parse_args()
 
-    SIZE = 60
-    DELTA_TIME = 0.05
-    N_PARTICLES = 100
-
-    registry = ParticleForceRegistry()
-    for _ in range(N_PARTICLES):
-        position = glm.vec3(
-            SIZE * random.random(), SIZE * random.random(), SIZE * random.random()
-        )
-        mass = 1000 * random.random()
-        forces = [
-            GravityForce(gravity=glm.vec3(0, -20, 0)),
-            DragForce(k1=0.1, k2=0.01),
-        ]
-        registry.add(particle=Particle(position, mass), forces=forces)
-
+    simulation = Simulation()
     anim = AnimatedPlot(
-        partial(update, registry=registry, size=SIZE, dt=DELTA_TIME),
+        partial(update, simulation=simulation),
         nframes=60,
-        scale=SIZE,
-        interval=int(DELTA_TIME * 1000),
+        scale=simulation.SCALE,
+        interval=50,
     )
 
     if args.output is None:
